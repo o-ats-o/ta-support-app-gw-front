2b8b9a066a30fee93031eb4e1405265378a58c0c
ats <atsushi.k214@gmail.com>
2024-07-08 00:41:10 +0900

    ボタンのUIを変更

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,44 @@ const App: React.FC = () => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <div className="flex flex-1 pt-16 p-2" style={{ minHeight: "100vh" }}>
-        <div className="w-2/5 p-4 border rounded-l-md border-[rgba(36,141,116,1)] mt-4 ml-2 group-list">
-          <div className="flex justify-between mb-4">
+      <div className="flex pt-16" style={{ minHeight: "100vh" }}>
+        <div className="w-2/5 p-4 border rounded-l-md border-[rgba(36,141,116,1)] mt-4 ml-4 group-list">
+          <div className="flex font-bold">
             <button
               onClick={() => setDisplayMode("発話回数")}
-              className={`px-4 py-2 w-full ${
-                displayMode === "発話回数" ? "bg-green-300" : "bg-white"
+              className={`px-4 py-2 w-full relative ${
+                displayMode === "発話回数"
+                  ? "text-[rgba(36,141,116,1)]"
+                  : "text-gray-500 hover:bg-gray-100"
               }`}
             >
-              発話回数
+              <span
+                className={`block pb-1 ${
+                  displayMode === "発話回数"
+                    ? "border-b-2 border-[rgba(36,141,116,1)]"
+                    : ""
+                }`}
+              >
+                発話回数
+              </span>
             </button>
             <button
               onClick={() => setDisplayMode("感情")}
-              className={`px-4 py-2 w-full ${
-                displayMode === "感情" ? "bg-green-300" : "bg-white"
+              className={`px-4 py-2 w-full relative ${
+                displayMode === "感情"
+                  ? "text-[rgba(36,141,116,1)]"
+                  : "text-gray-500 hover:bg-gray-100"
               }`}
             >
-              感情
+              <span
+                className={`block pb-1 ${
+                  displayMode === "感情"
+                    ? "border-b-2 border-[rgba(36,141,116,1)]"
+                    : ""
+                }`}
+              >
+                感情
+              </span>
             </button>
           </div>
           <GroupList
@@ -39,7 +59,7 @@ const App: React.FC = () => {
             displayMode={displayMode}
           />
         </div>
-        <div className="flex-1 p-4 overflow-y-auto border rounded-r-md border-[rgba(36,141,116,1)] mt-4 group-detail">
+        <div className="flex-1 p-4 overflow-y-auto border rounded-r-md border-[rgba(36,141,116,1)] mt-4 mr-2 group-detail">
           <GroupDetail groupName={selectedGroup} displayMode={displayMode} />
         </div>
       </div>
--- src/components/GroupDetail.tsx
+++ src/components/GroupDetail.tsx
@@ -135,29 +135,49 @@ const GroupDetail: React.FC<GroupDetailProps> = ({
       <h2 className="text-xl font-bold mb-2 underline text-[rgba(36,141,116,1)]">
         {groupName}
       </h2>
-      <div className="flex justify-center space-x-4 mb-4">
-        <button
-          onClick={() => setGraphMode("発話回数")}
-          className={`px-4 py-2 ${
-            graphMode === "発話回数" ? "bg-green-300" : "bg-white"
-          }`}
-        >
-          発話回数
-        </button>
-        <button
-          onClick={() => setGraphMode("感情")}
-          className={`px-4 py-2 ${
-            graphMode === "感情" ? "bg-green-300" : "bg-white"
-          }`}
-        >
-          感情
-        </button>
-      </div>
       <div className="mb-4">
         <h3 className="text-lg mb-1 font-bold text-[rgba(36,141,116,1)]">
           時間推移
         </h3>
         <div className="border border-gray-300 p-4">
+          <div className="flex justify-center space-x-4 mb-4 mx-24 font-bold">
+            <button
+              onClick={() => setGraphMode("発話回数")}
+              className={`px-4 py-2 w-full relative ${
+                graphMode === "発話回数"
+                  ? "text-[rgba(36,141,116,1)]"
+                  : "text-gray-500 hover:bg-gray-100"
+              }`}
+            >
+              <span
+                className={`block pb-1 ${
+                  graphMode === "発話回数"
+                    ? "border-b-2 border-[rgba(36,141,116,1)]"
+                    : ""
+                }`}
+              >
+                発話回数
+              </span>
+            </button>
+            <button
+              onClick={() => setGraphMode("感情")}
+              className={`px-4 py-2 w-full relative ${
+                graphMode === "感情"
+                  ? "text-[rgba(36,141,116,1)]"
+                  : "text-gray-500 hover:bg-gray-100"
+              }`}
+            >
+              <span
+                className={`block pb-1 ${
+                  graphMode === "感情"
+                    ? "border-b-2 border-[rgba(36,141,116,1)]"
+                    : ""
+                }`}
+              >
+                感情
+              </span>
+            </button>
+          </div>
           <Line data={data} options={options} />
         </div>
       </div>
